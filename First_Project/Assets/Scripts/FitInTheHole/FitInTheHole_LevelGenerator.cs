360101c74c54112158944f1214b69ecf0efe77aa
BioKotik <i.kachan@list.ru>
2020-05-01 14:06:30 +0300

    -

---
+++ First_Project/Assets/Scripts/FitInTheHole/FitInTheHole_LevelGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitInTheHole_LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject m_CubePrefab;
+    [SerializeField] private float m_BaseSpeed = 2f;
+    [SerializeField] private float m_WallDistance = 35f;
+
+    private float speed;
+    private FitInTheHole_Wall wall;
+
+    [SerializeField] private FitInTheHole_Template[] m_Templates;
+    [SerializeField] private Transform m_FigurePoint;
+
+    private FitInTheHole_Template[] templates;
+    private FitInTheHole_Template figure;
+
+    private void Start()
+    {
+        templates = new FitInTheHole_Template[m_Templates.Length];
+
+        for (int i = 0; i < m_Templates.Length; i++)
+        {
+            templates[i] = Instantiate(m_Templates[i]);
+            templates[i].gameObject.SetActive(false);
+            templates[i].transform.position = m_FigurePoint.position;
+        }
+
+        wall = new FitInTheHole_Wall(5, 5, m_CubePrefab);
+        SetUpTemplate();
+        wall.SetUpWall(figure, m_WallDistance);
+        speed = m_BaseSpeed;
+    }
+
+    private void SetUpTemplate()
+    {
+        if(figure != null)
+        {
+            figure.gameObject.SetActive(false);
+        }
+
+        var rand = Random.Range(0, templates.Length);
+        figure = templates[rand];
+        figure.gameObject.SetActive(true);
+        figure.SetUpRandomFigure();
+    }
+
+    private void Update()
+    {
+        wall.Parent.transform.Translate(Vector3.back * (Time.deltaTime * speed));
+
+        if(wall.Parent.transform.position.z > m_WallDistance * -1)
+        {
+            return;
+        }
+
+        wall.SetUpWall(figure, m_WallDistance);
+
+    }
+}
---
+++ First_Project/Assets/Scripts/FitInTheHole/FitInTheHole_Template.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitInTheHole_Template : MonoBehaviour
+{
+    [SerializeField] private Transform[] m_cubes;
+    [SerializeField] private Transform m_PlayerPosition;
+    [SerializeField] private Transform[] m_PositionsVariants;
+
+    private FitInTheHole_FigureTweener tweener;
+
+    public Transform m_CurrentTarget { get; private set; }
+
+    private int currentPosition = -1;
+
+    public Transform[] GetFigure()
+    {
+        var figure = new Transform[m_cubes.Length + 1];
+        m_cubes.CopyTo(figure, 0);
+        figure[figure.Length - 1] = m_CurrentTarget;
+        return figure;
+    }
+
+    public void SetUpRandomFigure()
+    {
+        int rand = Random.Range(0, m_PositionsVariants.Length);
+
+        for (int i = 0; i < m_PositionsVariants.Length; i++)
+        {
+            if(i == rand)
+            {
+                m_PositionsVariants[i].gameObject.SetActive(true);
+                m_CurrentTarget = m_PositionsVariants[i].transform;
+                continue;
+            }
+
+            m_PositionsVariants[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        if(tweener != null)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+    }
+
+    private void MoveLeft()
+    {
+        if(!IsMovementPossible(1))
+        {
+            return;
+        }
+        currentPosition += 1;
+        tweener = m_PlayerPosition.gameObject.AddComponent<FitInTheHole_FigureTweener>();
+        tweener.Tween(m_PlayerPosition.position, m_PositionsVariants[currentPosition].position);
+    }
+
+    private void MoveRight()
+    {
+        if (!IsMovementPossible(-1))
+        {
+            return;
+        }
+        currentPosition -= 1;
+        tweener = m_PlayerPosition.gameObject.AddComponent<FitInTheHole_FigureTweener>();
+        tweener.Tween(m_PlayerPosition.position, m_PositionsVariants[currentPosition].position);
+    }
+
+    private bool IsMovementPossible(int dir)
+    {
+        return currentPosition + dir >= 0 && currentPosition + dir < m_PositionsVariants.Length;
+    }
+}
